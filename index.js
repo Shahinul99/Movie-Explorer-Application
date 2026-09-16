caf319c1d1e65c586a29a1e0dfbe390883043930
Shahinul99 <rumaisaabrar99@gmail.com>
2026-09-16 18:56:15 -0400

    add index file

---
+++ index.js
@@ -0,0 +1,11 @@
+// function greetUser(name) {
+// return `Hello, ${name}! Welcome back.`;
+// }
+
+// const users = ["Alice", "Bob", "Charlie"];
+
+// for (let i = 0; i < users.length; i++) {
+// const greeting = greetUser(users[i]);
+
+// console.log(greeting);
+// // 
