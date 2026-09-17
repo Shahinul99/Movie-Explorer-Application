4fcd6e78bc45ecef4dfd8d4eacd04e1ef068caf1
Shahinul99 <rumaisaabrar99@gmail.com>
2026-09-17 19:25:02 -0400

    all

--- Movie-explorer-application/Movie-explorer-application/src/pages/Movies.jsx
+++ Movie-explorer-application/Movie-explorer-application/src/pages/Movies.jsx
@@ -24,7 +24,7 @@ export default function Movies() {
           data = await searchShows(query);
         }
         setShows(data);
-      } catch (error) {
+      } catch (err) {
         setError("Something went wrong. Please try again.");
       } finally {
         setLoading(false);
